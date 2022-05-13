bd0a06c0b3d03cd5c62b9ffb44aa4744e6911f0a
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2022-05-13 19:25:29 +0100

    Button component added

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Button from '@atlaskit/button';
+
+const ButtonPrimary = ({buttonName}) => {
+  return <Button appearance="primary">{buttonName}</Button>;
+};
+
+export default ButtonPrimary;
